b12f84e727bfae25859f7622e24701c68e940eb0
Osama Sehgol <osama.sehgol@gmail.com>
2016-02-10 16:50:31 -0500

    first instanceo f using twitter api client lib

---
+++ app.js
@@ -0,0 +1,38 @@
+var Twitter = require('twitter');// Step 1
+var Keys = require('./keys.js')(); // Step 1
+var http = require("http");
+var fs = require('fs'); // Using the filesystem module
+var httpServer = http.createServer(requestHandler);
+
+
+////////////////// Step 1 //////////////////////////////////
+var client = new Twitter({
+    consumer_key: Keys.consumer_key,
+    consumer_secret: Keys.consumer_secret,
+    token: Keys.access_token_key,
+    token_secret: Keys.access_token_secret
+});
+
+
+client.get('search/tweets', {q: 'i hate uber'}, function(error, tweets, response){
+   console.log(tweets);
+});
+
+function requestHandler(req, res) {
+	// Read index.html
+	fs.readFile(__dirname + '/index.html', 
+		// Callback function for reading
+		function (err, data) {
+			// if there is an error
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading whatever youre loading');
+			}
+			// Otherwise, send the data, the contents of the file
+			res.writeHead(200);
+			res.end(data);
+  		}
+  	);
+}
+
+httpServer.listen(8080);
